fe2654c8c67cf967a34cfdc7cfce7c4c6fe89842
Fabricio-Tomas <fabritomas25@gmail.com>
2022-08-11 20:36:10 -0300

    goup button

---
+++ docs/js/goup.js
@@ -0,0 +1,7 @@
+window.onscroll = function() {
+  if(window.pageYOffset > 500){
+    document.getElementById("goup-btn").style.display = "block";
+  }else{
+    document.getElementById("goup-btn").style.display = "none";
+  }
+};
