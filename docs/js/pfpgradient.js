517a8a91da459d82d7b5b121d42de4c91e0bdc79
Fabricio-Tomas <fabritomas25@gmail.com>
2022-09-01 17:01:13 -0300

    colors & cards layout

--- docs/js/pfpgradient.js
+++ docs/js/pfpgradient.js
@@ -2,7 +2,7 @@ var pfp = document.getElementById("pfp-big");
 var dir = 0.0; // linear gradient direction
 
 const interval = setInterval(function(){
-  pfp.style.background = "linear-gradient(#151515 0 0) padding-box, linear-gradient("+dir+"deg, #0DCAF0, #0d4df0) border-box";
+  pfp.style.background = "linear-gradient(#151515 0 0) padding-box, linear-gradient("+dir+"deg, var(--orange), var(--orange2)) border-box";
   dir += 1;
 
   // console.log("out- " + dir);
